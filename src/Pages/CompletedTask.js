eb5ff0cb38daa8575f98f50503cbbd41e1debf6c
nishattasnim08 <nishattasnim24258@gmail.com>
2022-07-01 00:26:17 +0600

    navber created and completed task route connected

---
+++ src/Pages/CompletedTask.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CompletedTask = () => {
+    return (
+        <div>
+            
+            <button className="btn">Completed Tasks!</button>
+        </div>
+    );
+};
+
+export default CompletedTask;
---
+++ src/Pages/Home/AddTask.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddTask = () => {
+    return (
+        <div>
+            <h1>Add Your Task</h1>
+        </div>
+    );
+};
+
+export default AddTask;
